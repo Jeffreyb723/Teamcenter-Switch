a296f7958a130dafb9a4b2271ae71c22439ac427
Jeffrey Bupp <jmbupp@gmail.com>
2021-02-11 23:06:57 -0500

    Add project files.

---
+++ Teamcenter Switch/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Teamcenter_Switch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(20, 20);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(217, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.Text = "Set to NX10.0 (Production)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(20, 57);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(178, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Set to NX1872 (UAT)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(18, 92);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(140, 92);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 146);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Teamcenter Switch/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Teamcenter_Switch
+{
+    public partial class Form1 : Form
+    {
+        private const string StartGeneric = @"C:\Siemens\Teamcenter11\UAT\rac\start_nxmanager.bat";
+        private const string Start1872 = @"C:\Siemens\Teamcenter11\UAT\rac\start_nxmanager1872.bat";
+        private const string Start10 = @"C:\Siemens\Teamcenter11\UAT\rac\start_nxmanager10.bat";
+
+        private const string Error = "Error";
+        private const string NoGeneric = "Cannot find start_nxmanager.bat, exiting application";
+        private const string NoSwitch =
+            "Cannot find start_nxmanager10.bat or start_nxmanager1872.bat. Please make sure one of these files is present.";
+
+        public Form1() => InitializeComponent();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                if (File.Exists(Start10))
+                {
+                    if (File.Exists(StartGeneric))
+                    {
+                        File.Move(StartGeneric, Start1872);
+                        File.Move(Start10, StartGeneric);
+                    }
+                    else
+                    {
+                        _ = MessageBox.Show(NoGeneric, Error, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else if (!File.Exists(Start1872))
+                {
+                    _ = MessageBox.Show(NoSwitch, Error, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                if (File.Exists(Start1872))
+                {
+                    if (File.Exists(StartGeneric))
+                    {
+                        File.Move(StartGeneric, Start10);
+                        File.Move(Start1872, StartGeneric);
+                    }
+                    else
+                    {
+                        _ = MessageBox.Show(NoGeneric, Error, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                else if (!File.Exists(Start10))
+                {
+                    _ = MessageBox.Show(NoSwitch, Error, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+            }
+
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e) => Close();
+    }
+}
